4aca4ba08b1cf56c09036cd15e190d8ef46d9ef4
Juan Martin Restrepo <tinto922@gmail.com>
2022-08-15 13:53:23 -0400

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 // your code here
+function prepend(numSpaces: number | string, inputString: string ): string {
+    // your code here
+    for(let i = 0; i < +numSpaces; i++){
+      inputString = "_" + inputString; 
+    }
+    return inputString; 
+  }
+  
+const button = document.querySelector("button") as HTMLButtonElement; 
+const numInput = document.getElementById("numInput") as HTMLInputElement; 
+const textInput = document.getElementById("textInput") as HTMLInputElement; 
+const divEle = document.querySelector("#answer") as HTMLDivElement; 
+
+button!.addEventListener("click", () => {
+    
+    let answer: string = prepend(numInput!.value, textInput!.value);
+    console.log(answer); 
+    divEle.innerHTML = answer; 
+})
